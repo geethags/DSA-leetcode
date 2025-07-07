12bfa6e182a2ee55aa641761eebd1920daa84897
geethags <60619075+geethags@users.noreply.github.com>
2025-07-07 12:19:57 +0530

    Time: 3 ms (54.44%) | Memory: 44.3 MB (89.08%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,17 +1,33 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set set = new HashSet<>();
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                char ele = board[i][j];
-                if(ele != '.'){
-                    if(!set.add(ele + " row "+ i) ||
-                    !set.add(ele + " col "+ j) || 
-                    !set.add(ele + " block " + i/3+j/3) ){
-                        return false;
-                    }
-                }
+        int N = 9;
+        HashSet<Character>[] rows = new HashSet[N];
+        HashSet<Character>[] cols = new HashSet[N];
+        HashSet<Character>[] boxes = new HashSet[N];
 
+        for(int i = 0; i < N; i++){
+            rows[i] = new HashSet<Character>();
+            cols[i] = new HashSet<Character>();
+            boxes[i] = new HashSet<Character>();
+        }
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                char ele = board[i][j];
+                if(ele == '.') continue;
+                if(rows[i].contains(ele)){
+                    return false;
+                }
+                rows[i].add(ele);
+                if(cols[j].contains(ele)){
+                    return false;
+                }
+                cols[j].add(ele);
+                int boxIndex = (i/3) * 3 + (j/3);
+                if(boxes[boxIndex].contains(ele)){
+                    return false;
+                }
+                boxes[boxIndex].add(ele);
             }
         }
         return true;
